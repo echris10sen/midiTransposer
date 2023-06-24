4c31795372a71ea5bdcbf57a864d3dfd14adf580
echris10sen <chr15016@byui.edu>
2023-06-23 21:54:34 -0700

    testing intranet...

--- serverDemo/serverTest.js
+++ serverDemo/serverTest.js
@@ -1,7 +1,7 @@
 async function logJSONData() {
     console.log("Loading...");
     data = {};
-    const response = await fetch("http://localhost:3000");
+    const response = await fetch("http://192.168.1.103:3000");
     const jsonData = await response.json();
     console.log(jsonData);
 };
