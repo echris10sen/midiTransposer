baf599748c5e642c258148c099f99bcb61164649
echris10sen <chr15016@byui.edu>
2023-06-24 20:02:55 -0700

    added error checking

--- coupDemo/scripts/functions/activate.js
+++ coupDemo/scripts/functions/activate.js
@@ -2,6 +2,7 @@ export function activate(numPlayers, totPlayers) {
     for (let index = 0; index < numPlayers; index++){
         const element = totPlayers[index];
         element.status = "active";
+        console.log(`Activating Player${totPlayers.playerNumber}`);
     };
     console.log(`${numPlayers} players have been activated`)
 }
